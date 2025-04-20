cf1d7e845d6667b484266516fb674679beaf8089
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-04-21 01:18:17 +0300

    For Fun

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,18 +7,35 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "red",
+    "yellow",
+    "purple",
+  ];
+  const users = [
+    { id: 1, firstName: "Vincent", lastName: "Kimani" },
+    { id: 2, firstName: "Dennis", lastName: "Macharia" },
   ];
 
+  const colorElements = colors.map((color) => {
+    return (
+      <li key={color} style={{ color: color }}>
+        {color}
+      </li>
+    );
+  });
+
+  const userHeadings = users.map((user) => {
+    return (
+      <h2 key={user.id}>
+        {user.firstName} {user.lastName}
+      </h2>
+    );
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
+      <ol>{colorElements}</ol>
+      <h2>{userHeadings}</h2>
     </div>
   );
 }
